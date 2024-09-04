cb74b5c832e59133e6558da292f6dbe457a31f44
Henry Zhang <henry.zhang@later.com>
2024-09-03 20:26:04 -0700

    implement Node

--- 02/src/monkey/ast/ast.go
+++ 02/src/monkey/ast/ast.go
@@ -14,3 +14,16 @@ type Expression interface {
 	Node
 	expressionNode()
 }
+
+// Every monkey program is a series of statements
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
